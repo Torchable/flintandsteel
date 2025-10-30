79ea36f5c1753846904ab371dd6d6a1ccb98df23
Colin Cheng <29507684+Torchable@users.noreply.github.com>
2025-10-30 14:56:13 -0400

    Update fasUtils.py

--- fasUtils.py
+++ fasUtils.py
@@ -302,31 +302,3 @@ def reset_transforms(nodes):
         cmds.setAttr(node + '.rotate', 0, 0, 0)
         cmds.setAttr(node + '.scale', 1, 1, 1)
 
-def riggroups(groups):
-    # User input for prefix name
-    x = cmds.promptDialog(title='RIG Group Setup', message='Insert Prefix Name')
-    if x == 'Confirm':
-        x = cmds.promptDialog(query=True, text=True)
-
-    elif x == 'dismiss':
-        return
-
-    # Checks if a rig group already exists and deletes it
-    rigListexist = cmds.ls('*_RIG')
-    if len(rigListexist) > 0:
-        cmds.delete(rigListexist)
-    groups(x)
-
-
-# Creation of groups
-def groups(x):
-    CTRL = cmds.group(em=True, n=x + '_CTRL_GRP')
-    GEO = cmds.group(em=True, n=x + '_GEO_GRP')
-    JNT = cmds.group(em=True, n=x + '_JNT_GRP')
-    notouch = cmds.group(em=True, n=x + '_notouch_GRP')
-    DEF = cmds.group(em=True, n=x + '_DEF_GRP')
-    IK = cmds.group(em=True, n=x + '_IK_GRP')
-
-    # Creation of rig group
-    RIG = cmds.group(CTRL, GEO, JNT, notouch, DEF, IK, n=x + '_RIG')
-
